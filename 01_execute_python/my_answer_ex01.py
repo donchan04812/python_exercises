0c750ccd890cfb6e00c653bf41d9a74a69a40ff6
donchan04812 <bdissvd.apollon@gmail.com>
2023-05-04 23:31:37 +0900

    test commit

---
+++ 01_execute_python/my_answer_ex01.py
@@ -0,0 +1,15 @@
+#%%
+def plus(x1, x2):
+    return x1 + x2
+#%%
+def plus_1(x):
+    one = 1
+    return one + x
+
+print(one)  # one is not defined!!
+#%%
+one = 1
+
+def plus_1(x):
+    return one + x
+# %%
